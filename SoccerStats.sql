03abfc60d62c1d7ef27101a295afcca42939bf16
Gloria Kuoh <gloriakuoh@outlook.com>
2021-09-03 15:22:38 -0400

    Updated SoccerStats SQL file

--- SoccerStats.sql
+++ SoccerStats.sql
@@ -1,6 +1,13 @@
-SELECT		plgoals.player, goals, avgvalue, position, (avgvalue/goals) AS CostPerGoal
-FROM		plgoals
-RIGHT JOIN 	plsalary
-ON			plgoals.player = plsalary.player
-WHERE		plsalary.position = 'Forward' AND plgoals.goals <> 0
-ORDER BY	CostPerGoal DESC;
+SELECT      plgoals.player, goals, avgvalue, position 
+FROM        plgoals 
+RIGHT JOIN  plsalary 
+ON          plgoals.player = plsalary.player 
+ORDER BY    Goals DESC;
+
+
+SELECT      plgoals.player, goals, avgvalue, position, (avgvalue/goals) AS CostPerGoal 
+FROM        plgoals 
+RIGHT JOIN  plsalary 
+ON          plgoals.player = plsalary.player 
+WHERE       plsalary.position = 'Forward' 
+AND         plgoals.goals <> 0 ORDER BY CostPerGoal DESC;
